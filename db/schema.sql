550b67857e939257bab890560880fc2e60290b9a
escowin <edwin@escowinart.com>
2022-04-25 16:48:22 -0500

    schema.sql:1-4 employees DROP DB, CREATE DB, & USE

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+
+USE employees;
+
 CREATE TABLE department(
     department_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
