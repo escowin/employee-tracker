69d0b20bb2a22c411a1fae1e724f781dfd28620d
escowin <edwin@escowinart.com>
2023-01-30 03:46:24 -0600

    seeds.sql | seeded database

--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,8 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10, 2) NOT NULL,
-    department_id INT NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
@@ -15,8 +15,8 @@ CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
